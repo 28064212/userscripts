78254a7d448e241a72e3505dd8f48c26a711bb7b
28064212 <28064212@users.noreply.github.com>
2014-05-08 09:59:38 +0100

    Create Boards.ie - Admin User Nav.user.js

---
+++ Boards.ie - Admin User Nav.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name Boards.ie - Admin User Nav
+// @namespace https://github.com/28064212/greasemonkey-scripts
+// @description Navigate between users in the Admin CP
+// @version 1.0
+// @downloadURL https://github.com/28064212/greasemonkey-scripts/raw/master/Boards.ie%20-%20Admin%20User%20Nav.user.js
+// @icon http://s3.amazonaws.com/uso_ss/icon/125952/large.png
+// @include http://www.boards.ie/admin/u.php*
+// ==/UserScript==
+
+function getParameterByName(name)
+{
+	name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+	var regexS = "[\\?&]" + name + "=([^&#]*)";
+	var regex = new RegExp(regexS);
+	var results = regex.exec(window.location.search);
+	if(results == null)
+	return "";
+	else
+	return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+var userid = parseInt(getParameterByName("u"));
+
+var prev = document.createElement("a");
+prev.innerHTML = "Previous";
+prev.href = "http://www.boards.ie/admin/u.php?do=edit&u=" + (userid - 1);
+var next = document.createElement("a");
+next.innerHTML = "Next";
+next.href = "http://www.boards.ie/admin/u.php?do=edit&u=" + (userid + 1);
+document.body.insertBefore(next, document.body.firstChild);
+document.body.insertBefore(prev, document.body.firstChild);
+
+window.addEventListener('keydown', pageNav, true);
+
+function pageNav(key)
+{
+	if(key.keyCode == 39 && (document.activeElement.nodeName != 'TEXTAREA' && document.activeElement.nodeName != 'INPUT'))
+		location.href = next.href;
+	else if(key.keyCode == 37 && (document.activeElement.nodeName != 'TEXTAREA' && document.activeElement.nodeName != 'INPUT'))
+		location.href = prev.href;
+}
