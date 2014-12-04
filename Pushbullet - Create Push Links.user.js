f60b377aca4958abf6f6bddff7d6738cc094802f
28064212 <28064212@users.noreply.github.com>
2014-12-04 17:14:27 +0000

    Fixed

--- Pushbullet - Create Push Links.user.js
+++ Pushbullet - Create Push Links.user.js
@@ -1,26 +1,25 @@
 // ==UserScript==
 // @name        Pushbullet - Create Push Links
 // @namespace   https://github.com/28064212/greasemonkey-scripts
-// @version 1.0
 // @downloadURL https://github.com/28064212/greasemonkey-scripts/raw/master/Pushbullet%20-%20Create%20Push%20Links.user.js
 // @description Add links to individual pushes
 // @include     https://www.pushbullet.com/
 // @include     https://www.pushbullet.com/?*
-// @version     1
+// @version     1.1
 // @grant       none
 // ==/UserScript==
 
 function addLinks()
 {
-	var divs = document.getElementsByClassName('top-line');
+	var divs = document.getElementsByClassName('push');
 	for(var i = 0; i < divs.length; i++)
 	{
-		console.log(divs[i].innerHTML)
+		//console.log(divs[i].innerHTML)
 		var st = divs[i].getElementsByClassName('small-text')[0];
 		if(st != null)
 		{
 			var a = document.createElement('a');
-			a.href = "?push_iden=" + st.parentNode.parentNode.parentNode.parentNode.id.replace('push_', '');
+			a.href = "?push_iden=" + divs[i].id.replace('push_', '');
 			a.innerHTML = st.innerHTML;
 			st.innerHTML = '';
 			st.appendChild(a);
@@ -30,7 +29,7 @@ function addLinks()
 function loader()
 {
 	addLinks();
-	var target = document.getElementsByClassName('physics')[0];
+	var target = document.getElementsByClassName('push_cards')[0];
 	var observer = new MutationObserver(function(mutations) {
 			addLinks();
 		});
