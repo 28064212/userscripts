7987502046e34c5a14cd654ae65a65a1bb507d98
28064212 <28064212@users.noreply.github.com>
2014-12-04 16:50:50 +0000

    don't fire if shiftkey pressed

--- Gmail - Open Links in Email.user.js
+++ Gmail - Open Links in Email.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Gmail - Open Links in Email
 // @namespace https://github.com/28064212/greasemonkey-scripts
-// @version 1.0
+// @version 1.1
 // @downloadURL https://github.com/28064212/greasemonkey-scripts/raw/master/Gmail%20-%20Open%20Links%20in%20Email.user.js
 // @description Use the number keys (0-9) to open links in first email in conversation that has been opened. 1 opens first link, etc.
 // @include https://mail.google.com/*
@@ -17,6 +17,7 @@ function keyShortcuts(key)
 	var code = key.keyCode;
 	var ctrl = key.ctrlKey;
 	var alt = key.altKey;
-	if(!ctrl && !alt && code >= 48 && code <= 57 && document.getElementsByClassName('a3s')[0] != null)
+	var shift = key.shiftKey;
+	if(!ctrl && !alt && !shift && code >= 48 && code <= 57 && document.getElementsByClassName('a3s')[0] != null)
 		window.open(document.getElementsByClassName('a3s')[0].getElementsByTagName("a")[code == 48 ? 10 : code - 49]);
 }
