5defc639ba95d3cb3142eea4d7f2cb96e263688e
28064212 <28064212@users.noreply.github.com>
2015-09-10 12:09:10 +0100

    Update Google - Keyboard Shortcuts.user.js

--- Google - Keyboard Shortcuts.user.js
+++ Google - Keyboard Shortcuts.user.js
@@ -2,8 +2,8 @@
 // @name        Google - Keyboard Shortcuts
 // @namespace   https://github.com/28064212/greasemonkey-scripts
 // @downloadURL https://github.com/28064212/greasemonkey-scripts/raw/master/Google%20-%20Keyboard%20Shortcuts.user.js
-// @include		/^https?://(www\.)?google\..*/
-// @version     1.0.1
+// @include		/^https?://(www\.)?google\..*/search.*/
+// @version     1.0.2
 // @description	a/z for up/down, q to open, ctrl-space to focus search
 // @grant		GM_addStyle
 // ==/UserScript==
