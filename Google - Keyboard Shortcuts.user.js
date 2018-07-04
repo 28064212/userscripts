dd43063754ff87f123a3acfe4a2068731e7c6993
28064212 <28064212@users.noreply.github.com>
2018-07-04 13:51:34 +0100

    Bugfix

--- Google - Keyboard Shortcuts.user.js
+++ Google - Keyboard Shortcuts.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/28064212/greasemonkey-scripts
 // @downloadURL https://github.com/28064212/greasemonkey-scripts/raw/master/Google%20-%20Keyboard%20Shortcuts.user.js
 // @include	/^https?://(www\.)?google\..*/search.*/
-// @version     1.2
+// @version     1.2.1
 // @description	a/z for up/down, q to open, ctrl-space to focus search
 // ==/UserScript==
 
@@ -63,7 +63,7 @@ function keyShortcuts(key)
 	if(!intext && (code == 65 || code == 90))
 	{
 		// a/z - up/down
-		var list = document.querySelectorAll('.bkWMgd .rc, .bkWMgd .sld.vsc, .srg .g, ._NId .g');
+		var list = document.querySelectorAll('.bkWMgd .rc, .bkWMgd .sld.vsc, .srg .rc, ._NId .rc');
 		if(hl != null)
 			hl.classList.remove('highlight436255');
 		if(hl == null || !isElementInViewport(hl))
