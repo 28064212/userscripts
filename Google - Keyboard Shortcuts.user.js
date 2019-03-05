2fa005290cd3fd72fa764c57bd17114a9d10b3fd
28064212 <28064212@users.noreply.github.com>
2019-03-05 10:17:38 +0000

    reformat, exclude image search

--- Google - Keyboard Shortcuts.user.js
+++ Google - Keyboard Shortcuts.user.js
@@ -3,25 +3,26 @@
 // @namespace   https://github.com/28064212/greasemonkey-scripts
 // @downloadURL https://github.com/28064212/greasemonkey-scripts/raw/master/Google%20-%20Keyboard%20Shortcuts.user.js
 // @include	/^https?://(www\.)?google\..*/search.*/
-// @version     1.2.1
+// @version     1.3
 // @description	a/z for up/down, q to open, ctrl-space to focus search
 // ==/UserScript==
 
 //from: https://stackoverflow.com/a/46285637
 function addGlobalStyle(css) {
-    var head, style;
-    head = document.getElementsByTagName('head')[0];
-    if (!head) {
-	console.log("a"); return; }
+	var head, style;
+	head = document.getElementsByTagName('head')[0];
+	if (!head) {
+		console.log("a");
+		return;
+	}
 	console.log("b");
-    style = document.createElement('style');
-    style.type = 'text/css';
-    //style.innerHTML = css.replace(/;/g, ' !important;');
+	style = document.createElement('style');
+	style.type = 'text/css';
+	//style.innerHTML = css.replace(/;/g, ' !important;');
 	style.innerHTML = css
-    head.appendChild(style);
+	head.appendChild(style);
 }
-if(window.top == window.self)
-{
+if (window.top == window.self) {
 	addGlobalStyle("\
 		.highlight436255 {\n\
 			border:red solid 1px !important;\n\
@@ -53,76 +54,61 @@ p - 80
 Del - 46
 ` - 223
  */
-function keyShortcuts(key)
-{
+function keyShortcuts(key) {
 	var code = key.keyCode;
 	var ctrl = key.ctrlKey;
 	var alt = key.altKey;
 	var intext = (document.activeElement.nodeName == 'TEXTAREA' || document.activeElement.nodeName == 'INPUT');
+	var isch = location.toString().indexOf('tbm=isch') >= 0; // image search
 	var hl = document.getElementsByClassName('highlight436255')[0];
-	if(!intext && (code == 65 || code == 90))
-	{
+	if (!isch && !intext && (code == 65 || code == 90)) {
 		// a/z - up/down
 		var list = document.querySelectorAll('.bkWMgd .rc, .bkWMgd .sld.vsc, .srg .rc, ._NId .rc');
-		if(hl != null)
+		if (hl != null)
 			hl.classList.remove('highlight436255');
-		if(hl == null || !isElementInViewport(hl))
+		if (hl == null || !isElementInViewport(hl))
 			index = -1;
-		if(index == -1)
-		{
-			if(code == 65)
-			{
-				if(ctrl)
+		if (index == -1) {
+			if (code == 65) {
+				if (ctrl)
 					index = 0;
-				else
-				{
-					for(var j = list.length - 1; j > 0 && index == -1; j--)
-					{
-						if(isElementInViewport(list[j]))
+				else {
+					for (var j = list.length - 1; j > 0 && index == -1; j--) {
+						if (isElementInViewport(list[j]))
 							index = j;
 					}
-					if(index == -1)
+					if (index == -1)
 						index = list.length - 1;
 				}
-			}
-			else if(code == 90)
-			{
-				if(ctrl)
+			} else if (code == 90) {
+				if (ctrl)
 					index = list.length - 1;
-				else
-				{
-					for(var j = 0; j < list.length && index == -1; j++)
-					{
-						if(isElementInViewport(list[j]))
+				else {
+					for (var j = 0; j < list.length && index == -1; j++) {
+						if (isElementInViewport(list[j]))
 							index = j;
 					}
-					if(index == -1)
+					if (index == -1)
 						index = 0;
 				}
 			}
-		}
-		else if(code == 65 && index > 0)
-		{
-			if(ctrl)
+		} else if (code == 65 && index > 0) {
+			if (ctrl)
 				index = 0;
 			else
 				index--;
-		}
-		else if(code == 90 && index < list.length - 1)
-		{
-			if(ctrl)
+		} else if (code == 90 && index < list.length - 1) {
+			if (ctrl)
 				index = list.length - 1;
 			else
 				index++;
 		}
 		list[index].classList.add('highlight436255');
 		hl = document.getElementsByClassName('highlight436255')[0];
-		if(!isElementInViewport(hl))
+		if (!isElementInViewport(hl))
 			hl.scrollIntoView(code == 90);
 		key.preventDefault();
-	}
-	else if(!intext && !ctrl && !alt && code == 81 && hl != null)
-	{
+	} else if (!isch && !intext && !ctrl && !alt && code == 81 && hl != null) {
 		// q - open in new tab
 		//var evt = document.createEvent("MouseEvents");
 		//evt.initMouseEvent("click", true, true, window, 1, 0, 0, 0, 0, true, false, false, false, 0, null);
@@ -130,26 +116,21 @@ function keyShortcuts(key)
 		//hl.parentNode.parentNode.parentNode.parentNode.getElementsByClassName('inlineFrame')[0].getElementsByTagName('a')[0].dispatchEvent(evt);
 		window.open(hl.getElementsByTagName('a')[0]);
 		//hl.classList.add('highlight436255');
-	}
-	else if(!intext && !ctrl && !alt && code == 39)
-	{
+	} else if (!isch && !intext && !ctrl && !alt && code == 39) {
 		window.location.href = document.getElementById('pnnext');
-	}
-	else if(!intext && !ctrl && !alt && code == 37)
-	{
+	} else if (!isch && !intext && !ctrl && !alt && code == 37) {
 		window.location.href = document.getElementById('pnprev');
-	}
-	else if(!intext && ctrl && code == 32)
-	{
+	} else if (!intext && ctrl && code == 32) {
 		document.getElementsByName('q')[0].select();
 	}
 }
-function isElementInViewport (el) {
+
+function isElementInViewport(el) {
 	var rect = el.getBoundingClientRect();
 	return (
-			rect.top >= 0 &&
-			rect.left >= 0 &&
-			rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-			rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+		rect.top >= 0 &&
+		rect.left >= 0 &&
+		rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		rect.right <= (window.innerWidth || document.documentElement.clientWidth)
 	);
 }
