9a7e9ff2eb6490b94a198f6328efa89510ae5cc2
28064212 <28064212@users.noreply.github.com>
2019-09-13 10:55:42 +0100

    Fix for updated border style

--- Boards.ie - Total Ignore.user.js
+++ Boards.ie - Total Ignore.user.js
@@ -2,53 +2,45 @@
 // @name Boards.ie - Total Ignore
 // @namespace https://github.com/28064212/greasemonkey-scripts
 // @description Hides ignored users' posts totally, as well as text from posts that quote them (leaves option to display those posts)
-// @version 1.2
+// @version 1.3
 // @downloadURL https://github.com/28064212/greasemonkey-scripts/raw/master/Boards.ie%20-%20Total%20Ignore.user.js
 // @icon http://s3.amazonaws.com/uso_ss/icon/125952/large.png
 // @include http://www.boards.ie/vbulletin/showthread.php*
 // @include https://www.boards.ie/vbulletin/showthread.php*
+// @grant GM_xmlhttpRequest
 // ==/UserScript==
 
-//v1.1 - new HTML Parser due to problems with dark skin
-//v1.2 - move to github, return if not top window
-
-if(window.top !== window.self)
+if (window.top !== window.self)
 	return;
 
 var SCORCHED_EARTH = false; // if true, totally remove posts that quote someone on ignore. Otherwise, they're just hidden, with an option to "show post"
 
 var links = document.getElementsByTagName("a");
-for(var i = 0; i < links.length; i++)
-{
-	if(links[i].innerHTML == "Remove user from ignore list")
+for (var i = 0; i < links.length; i++) {
+	if (links[i].innerHTML == "Remove user from ignore list")
 		links[i].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style.display = "none";
 }
 
 GM_xmlhttpRequest({
 	method: "GET",
-	url: "http://www.boards.ie/vbulletin/profile.php?do=ignorelist",
-	onload: function(response){
+	url: "https://www.boards.ie/vbulletin/profile.php?do=ignorelist",
+	onload: function (response) {
 		//from https://developer.mozilla.org/en-US/docs/Code_snippets/HTML_to_DOM
 		var parsedhtml = document.implementation.createHTMLDocument("example");
 		parsedhtml.documentElement.innerHTML = response.responseText;
-		if(parsedhtml.getElementById("ignorelist") != null)
-		{
+		if (parsedhtml.getElementById("ignorelist") != null) {
 			var ignoreduserslinks = parsedhtml.getElementById("ignorelist").getElementsByTagName("a");
 			var ignoredusers = new Array();
-			for(var i = 0; i < ignoreduserslinks.length; i++)
+			for (var i = 0; i < ignoreduserslinks.length; i++)
 				ignoredusers.push(ignoreduserslinks[i].innerHTML);
 			var quotes = document.getElementsByClassName("alt2");
-			for(var i = 0; i < quotes.length; i++)
-			{
-				if(quotes[i].style.borderStyle == "inset" && quotes[i].getElementsByTagName("strong")[0] != null 
-						&& ignoredusers.indexOf(quotes[i].getElementsByTagName("strong")[0].innerHTML) != -1)
-				{
-					if(SCORCHED_EARTH)
-					{
+			for (var i = 0; i < quotes.length; i++) {
+				if (quotes[i].style.border.indexOf("inset") != -1 && quotes[i].getElementsByTagName("strong")[0] != null
+					&& ignoredusers.indexOf(quotes[i].getElementsByTagName("strong")[0].innerHTML) != -1) {
+					if (SCORCHED_EARTH) {
 						quotes[i].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style.display = "none";
 					}
-					else
-					{
+					else {
 						var post = quotes[i].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
 						var extradiv = document.createElement("div");
 						extradiv.innerHTML = post.innerHTML;
@@ -58,7 +50,7 @@ GM_xmlhttpRequest({
 						show.innerHTML = "---This post quotes ignored user, click to show---";
 						show.style.textDecoration = "underline";
 						show.style.cursor = "pointer";
-						show.onclick = function(){
+						show.onclick = function () {
 							this.nextSibling.style.visibility = "visible";
 							this.style.display = "none";
 						};
