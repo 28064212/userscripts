ae3f998ed4d37dd2724deae54d5836fe017d1ff8
28064212 <28064212@users.noreply.github.com>
2019-10-07 16:07:14 +0100

    Add check for route existing

--- MapMyRun Keyboard Shortcuts.user.js
+++ MapMyRun Keyboard Shortcuts.user.js
@@ -4,7 +4,7 @@
 // @match       https://www.mapmyrun.com/routes/view/*
 // @include     https://www.mapmyrun.com/workout/*
 // @downloadURL https://github.com/28064212/userscripts/raw/master/MapMyRun%20Keyboard%20Shortcuts.user.js
-// @version     1.1
+// @version     1.2
 // @grant       none
 // ==/UserScript==
 if (window.top == window.self) {
@@ -36,7 +36,8 @@ function keyShortcuts(key) {
 	else if (code == 65 && ctrl) {
 		// Ctrl+A on workout view to open route and edit workout
 		key.preventDefault();
-		window.open(document.getElementById("route_info").getElementsByTagName("a")[0].href);
+		if (document.getElementById("route_info") !== null)
+			window.open(document.getElementById("route_info").getElementsByTagName("a")[0].href);
 		document.location = document.getElementById("workout_details_edit").href;
 	}
 }
