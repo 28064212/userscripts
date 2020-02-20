829934a8e738eeb46b367c075f4a856bd97002bd
28064212 <28064212@users.noreply.github.com>
2020-02-20 11:17:13 +0000

    New method for deleting routes

--- MapMyRun Keyboard Shortcuts.user.js
+++ MapMyRun Keyboard Shortcuts.user.js
@@ -3,8 +3,9 @@
 // @namespace   https://github.com/28064212/greasemonkey-scripts
 // @match       https://www.mapmyrun.com/routes/view/*
 // @include     https://www.mapmyrun.com/workout/*
+// @match       https://www.mapmyrun.com/routes/my_routes/
 // @downloadURL https://github.com/28064212/userscripts/raw/master/MapMyRun%20Keyboard%20Shortcuts.user.js
-// @version     1.2
+// @version     1.3
 // @grant       none
 // ==/UserScript==
 if (window.top == window.self) {
@@ -29,15 +30,30 @@ function keyShortcuts(key) {
 	}
 	else if (code == 68 && ctrl) {
 		// Ctrl+D on route view to delete
-		var evt = new MouseEvent("click", { bubbles: true, cancelable: true });
-		document.getElementsByClassName("delete_route")[0].dispatchEvent(evt);
-		key.preventDefault();
+		// var evt = new MouseEvent("click", { bubbles: true, cancelable: true });
+		// document.getElementsByClassName("delete_route")[0].dispatchEvent(evt);
+		// key.preventDefault();
 	}
 	else if (code == 65 && ctrl) {
-		// Ctrl+A on workout view to open route and edit workout
+		// Ctrl+A on workout view to edit workout
 		key.preventDefault();
-		if (document.getElementById("route_info") !== null)
-			window.open(document.getElementById("route_info").getElementsByTagName("a")[0].href);
+		// if (document.getElementById("route_info") !== null)
+			// window.open(document.getElementById("route_info").getElementsByTagName("a")[0].href);
 		document.location = document.getElementById("workout_details_edit").href;
+	}
+	else if (code >= 49 && code <= 57) {
+		// 1-9 to delete routes
+		var dels = Array.prototype.slice.call(document.getElementsByTagName('a')).filter(el => el.textContent.trim() === 'Delete');
+		var num = code - 49;
+		if(dels.length > 0 && num + 1 <= dels.length) {
+			dels[num].click();
+		}
+	}
+	else if (code == 13) {
+		// Enter to confirm deletion of route
+		var oks = Array.prototype.slice.call(document.getElementsByTagName('span')).filter(el => el.textContent.trim() === 'OK');
+		if(oks.length > 0) {
+			oks[0].click();
+		}
 	}
 }
