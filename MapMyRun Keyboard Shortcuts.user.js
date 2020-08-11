33f96dd4bbaf6909fd5cd6d2fee32872c1e38226
28064212 <28064212@users.noreply.github.com>
2020-08-11 13:40:28 +0100

    Adapting to changes on site

--- MapMyRun Keyboard Shortcuts.user.js
+++ MapMyRun Keyboard Shortcuts.user.js
@@ -2,12 +2,14 @@
 // @name        MapMyRun Keyboard Shortcuts
 // @namespace   https://github.com/28064212/greasemonkey-scripts
 // @match       https://www.mapmyrun.com/routes/view/*
-// @include     https://www.mapmyrun.com/workout/*
+// @match       https://www.mapmyrun.com/workout/*
 // @match       https://www.mapmyrun.com/routes/my_routes/
+// @match       https://www.mapmyrun.com/activity_feed*
 // @downloadURL https://github.com/28064212/userscripts/raw/master/MapMyRun%20Keyboard%20Shortcuts.user.js
-// @version     1.3
+// @version     1.4
 // @grant       none
 // ==/UserScript==
+
 if (window.top == window.self) {
 	window.addEventListener('keydown', keyShortcuts, true);
 }
@@ -17,42 +19,50 @@ function keyShortcuts(key) {
 	var alt = key.altKey;
 	if (code == 32 && ctrl) {
 		// Ctrl+Space on workout edit to clear route and focus
-		var evt = new MouseEvent("click", { bubbles: true, cancelable: true });
-		document.getElementsByClassName("inner-2cIvZ")[0].dispatchEvent(evt);
-		document.getElementsByClassName("routeSelector-25BXq")[0].getElementsByTagName("input")[0].focus();
+		document.getElementsByTagName('button')[0].click();
+		document.getElementsByClassName('routeSelector-25BXq')[0].getElementsByTagName("input")[0].focus();
+		key.preventDefault();
+	}
+	else if (code == 65 && ctrl) {
+		// Ctrl+A to focus on Notes
+		document.getElementsByClassName('additionalStats-3QChq')[0].click();
+		document.getElementsByClassName('input-2iIsn')[0].focus();
+		window.setTimeout(function(){window.scrollTo(0,document.body.scrollHeight);},500)
 		key.preventDefault();
 	}
 	else if (code == 83 && ctrl) {
 		// Ctrl+S on workout edit to save
-		var evt = new MouseEvent("click", { bubbles: true, cancelable: true });
-		document.getElementsByClassName("saveButton-3fynd")[0].dispatchEvent(evt);
+		document.getElementsByClassName('saveButtonContainer-3_WXK')[0].getElementsByTagName('button')[0].click();
 		key.preventDefault();
-	}
-	else if (code == 68 && ctrl) {
-		// Ctrl+D on route view to delete
-		// var evt = new MouseEvent("click", { bubbles: true, cancelable: true });
-		// document.getElementsByClassName("delete_route")[0].dispatchEvent(evt);
-		// key.preventDefault();
-	}
-	else if (code == 65 && ctrl) {
-		// Ctrl+A on workout view to edit workout
-		key.preventDefault();
-		// if (document.getElementById("route_info") !== null)
-			// window.open(document.getElementById("route_info").getElementsByTagName("a")[0].href);
-		document.location = document.getElementById("workout_details_edit").href;
 	}
 	else if (code >= 49 && code <= 57) {
-		// 1-9 to delete routes
-		var dels = Array.prototype.slice.call(document.getElementsByTagName('a')).filter(el => el.textContent.trim() === 'Delete');
-		var num = code - 49;
-		if(dels.length > 0 && num + 1 <= dels.length) {
-			dels[num].click();
+		if (window.location.href.indexOf('/workout/') != -1) {
+			// 1-9 to view workout
+			document.location = document.getElementById("workout_details_edit").href;
+		}
+		else if (window.location.href.indexOf('activity_feed') != -1) {
+			// 1-9 to open workouts
+			key.preventDefault();
+			var workouts = document.getElementsByClassName('row-ZtOZv');
+			var num = code - 49;
+			if (workouts.length > 0 && num + 1 <= workouts.length) {
+				var evt = new MouseEvent("click", { bubbles: true, cancelable: true, ctrlKey: ctrl });
+				workouts[num].getElementsByTagName("a")[0].dispatchEvent(evt);
+			}
+		}
+		else {
+			// 1-9 to delete routes
+			var dels = Array.prototype.slice.call(document.getElementsByTagName('a')).filter(el => el.textContent.trim() === 'Delete');
+			var num = code - 49;
+			if (dels.length > 0 && num + 1 <= dels.length) {
+				dels[num].click();
+			}
 		}
 	}
 	else if (code == 13) {
 		// Enter to confirm deletion of route
 		var oks = Array.prototype.slice.call(document.getElementsByTagName('span')).filter(el => el.textContent.trim() === 'OK');
-		if(oks.length > 0) {
+		if (oks.length > 0) {
 			oks[0].click();
 		}
 	}
