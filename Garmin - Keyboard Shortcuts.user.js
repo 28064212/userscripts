a613f235c6bafbb3d2251eef45667ae3aa3830fd
28064212 <28064212@users.noreply.github.com>
2021-02-12 17:24:57 +0000

    Create Garmin - Keyboard Shortcuts.user.js

---
+++ Garmin - Keyboard Shortcuts.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name        Garmin - Keyboard Shortcuts
+// @namespace   https://github.com/28064212/greasemonkey-scripts
+// @match       https://connect.garmin.com/*
+// @downloadURL https://github.com/28064212/userscripts/raw/master/Garmin%20-%20Keyboard%20Shortcuts.user.js
+// @version     1.0
+// @grant       none
+// ==/UserScript==
+
+if (window.top == window.self) {
+	window.addEventListener('keydown', keyShortcuts, true);
+}
+function keyShortcuts(key) {
+	var code = key.keyCode;
+	var ctrl = key.ctrlKey;
+	var alt = key.altKey;
+	if (code >= 49 && code <= 57 && window.location.href.includes('/activities')) {
+		// 1-9 to open workouts
+		key.preventDefault();
+		var workouts = document.querySelectorAll('.activity-name-edit .inline-edit-target');
+		var num = code - 49;
+		if (workouts.length > 0 && num + 1 <= workouts.length) {
+			var evt = new MouseEvent("click", { bubbles: true, cancelable: true, ctrlKey: ctrl });
+			workouts[num].dispatchEvent(evt);
+		}
+	}
+}
