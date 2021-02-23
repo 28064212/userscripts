7030e30e55c3bd47c0058da08200cd4ed94cef65
28064212 <28064212@users.noreply.github.com>
2021-02-23 16:49:32 +0000

    Copy to clipboard

--- Garmin - Keyboard Shortcuts.user.js
+++ Garmin - Keyboard Shortcuts.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name        Garmin - Keyboard Shortcuts
-// @namespace   https://github.com/28064212/greasemonkey-scripts
+// @namespace   https://github.com/28064212/userscripts
 // @match       https://connect.garmin.com/*
 // @downloadURL https://github.com/28064212/userscripts/raw/master/Garmin%20-%20Keyboard%20Shortcuts.user.js
-// @version     1.0
+// @version     1.1
 // @grant       none
 // ==/UserScript==
 
@@ -20,6 +20,7 @@ function keyShortcuts(key) {
 		var workouts = document.querySelectorAll('.activity-name-edit .inline-edit-target');
 		var num = code - 49;
 		if (workouts.length > 0 && num + 1 <= workouts.length) {
+			navigator.clipboard.writeText(workouts[num].href);
 			var evt = new MouseEvent("click", { bubbles: true, cancelable: true, ctrlKey: ctrl });
 			workouts[num].dispatchEvent(evt);
 		}
