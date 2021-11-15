1bc0e5b0887ef02a5e91ad9cf486971061bdd37f
28064212 <halligf2@gmail.com>
2021-11-15 15:44:56 +0000

    use Enter for weight

--- Garmin - Keyboard Shortcuts.user.js
+++ Garmin - Keyboard Shortcuts.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/28064212/userscripts
 // @match       https://connect.garmin.com/*
 // @downloadURL https://github.com/28064212/userscripts/raw/master/Garmin%20-%20Keyboard%20Shortcuts.user.js
-// @version     1.1
+// @version     1.2
 // @grant       none
 // ==/UserScript==
 
@@ -11,18 +11,37 @@ if (window.top == window.self) {
 	window.addEventListener('keydown', keyShortcuts, true);
 }
 function keyShortcuts(key) {
-	var code = key.keyCode;
-	var ctrl = key.ctrlKey;
-	var alt = key.altKey;
+	const code = key.keyCode;
+	const ctrl = key.ctrlKey;
+	const alt = key.altKey;
 	if (code >= 49 && code <= 57 && window.location.href.includes('/activities')) {
 		// 1-9 to open workouts
 		key.preventDefault();
-		var workouts = document.querySelectorAll('.activity-name-edit .inline-edit-target');
-		var num = code - 49;
+		const workouts = document.querySelectorAll('.activity-name-edit .inline-edit-target');
+		const num = code - 49;
 		if (workouts.length > 0 && num + 1 <= workouts.length) {
 			navigator.clipboard.writeText(workouts[num].href);
-			var evt = new MouseEvent("click", { bubbles: true, cancelable: true, ctrlKey: ctrl });
+			const evt = new MouseEvent("click", { bubbles: true, cancelable: true, ctrlKey: ctrl });
 			workouts[num].dispatchEvent(evt);
+		}
+	}
+	else if (code == 13 && document.querySelector('.weight')) {
+		// Enter - add weight
+		const buttons = document.querySelectorAll("button");
+		const save = document.querySelector("[class*='WeightPage_actionButtonsContainer'] button:last-child");
+		if (save.disabled) {
+			for (let b of buttons) {
+				if (b.innerText == "Add Weight") {
+					b.focus();
+					b.click();
+					break;
+				}
+			}
+			const input = document.querySelector("[class^='WeightPage_weightInputContainer__1yJBt'] input");
+			input.focus();
+		}
+		else {
+			save.click();
 		}
 	}
 }
