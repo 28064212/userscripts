9f4d253c2025297de72d047b2b8fa442abdb426c
28064212 <halligf2@gmail.com>
2021-11-21 18:10:41 +0000

    fixes for detecting intext

--- MyFitnessPal - Keyboard Shortcuts.user.js
+++ MyFitnessPal - Keyboard Shortcuts.user.js
@@ -6,7 +6,7 @@
 // @include /^https?://(www\.)?myfitnesspal\.com/user/.*/diary/add\?.*/
 // @match https://www.myfitnesspal.com/food/diary*
 // @match https://www.myfitnesspal.com/food/search
-// @version 1.4.1
+// @version 1.4.2
 // @description	a/z for up/down, q to select, w to select quantity, shift+d delete all
 // ==/UserScript==
 
@@ -35,7 +35,7 @@ function keyShortcuts(key) {
 	const ctrl = key.ctrlKey;
 	const alt = key.altKey;
 	const shift = key.shiftKey;
-	const intext = (document.activeElement.nodeName == 'TEXTAREA' || (document.activeElement.nodeName == 'INPUT' && document.activeElement.type != "checkbox"));
+	const intext = (document.activeElement.nodeName == 'TEXTAREA' || (document.activeElement.nodeName == 'INPUT' && document.activeElement.type != "checkbox" && document.activeElement.type != "submit"));
 	let hl = document.querySelector('.highlight436255');
 	if (ctrl && (code == 32)) {
 		document.querySelector("#search").focus();
@@ -96,65 +96,67 @@ function keyShortcuts(key) {
 	}
 	else if (!ctrl && (code == 65 || code == 90)) {
 		// a/z
-		key.preventDefault();
-		let list = [];
-		if (document.getElementById('recipes').style.display != 'none')
-			list = document.querySelectorAll('#recipes .favorite');
-		else if (document.getElementById('frequent').style.display != 'none')
-			list = document.querySelectorAll('#frequent .favorite');
-		else if (document.getElementById('recent').style.display != 'none')
-			list = document.querySelectorAll('#recent .favorite');
-		else if (document.getElementById('meals').style.display != 'none')
-			list = document.querySelectorAll('#meals .favorite');
-		else if (document.getElementById('my_foods').style.display != 'none')
-			list = document.querySelectorAll('#my_foods .favorite');
-		if (hl != null)
-			hl.classList.remove('highlight436255');
-		if (hl == null || !isElementInViewport(hl))
-			index = -1;
-		if (index == -1) {
-			if (code == 65) {
+		if (!intext || (document.activeElement && document.activeElement.id.startsWith("favorites_"))) {
+			key.preventDefault();
+			let list = [];
+			if (document.getElementById('recipes').style.display != 'none')
+				list = document.querySelectorAll('#recipes .favorite');
+			else if (document.getElementById('frequent').style.display != 'none')
+				list = document.querySelectorAll('#frequent .favorite');
+			else if (document.getElementById('recent').style.display != 'none')
+				list = document.querySelectorAll('#recent .favorite');
+			else if (document.getElementById('meals').style.display != 'none')
+				list = document.querySelectorAll('#meals .favorite');
+			else if (document.getElementById('my_foods').style.display != 'none')
+				list = document.querySelectorAll('#my_foods .favorite');
+			if (hl != null)
+				hl.classList.remove('highlight436255');
+			if (hl == null || !isElementInViewport(hl))
+				index = -1;
+			if (index == -1) {
+				if (code == 65) {
+					if (shift)
+						index = 0;
+					else {
+						for (let j = list.length - 1; j > 0 && index == -1; j--) {
+							if (isElementInViewport(list[j]))
+								index = j;
+						}
+						if (index == -1)
+							index = list.length - 1;
+					}
+				}
+				else if (code == 90) {
+					if (shift)
+						index = list.length - 1;
+					else {
+						for (let j = 0; j < list.length && index == -1; j++) {
+							if (isElementInViewport(list[j]))
+								index = j;
+						}
+						if (index == -1)
+							index = 0;
+					}
+				}
+			}
+			else if (code == 65 && index > 0) {
 				if (shift)
 					index = 0;
-				else {
-					for (let j = list.length - 1; j > 0 && index == -1; j--) {
-						if (isElementInViewport(list[j]))
-							index = j;
-					}
-					if (index == -1)
-						index = list.length - 1;
-				}
+				else
+					index--;
 			}
-			else if (code == 90) {
+			else if (code == 90 && index < list.length - 1) {
 				if (shift)
 					index = list.length - 1;
-				else {
-					for (let j = 0; j < list.length && index == -1; j++) {
-						if (isElementInViewport(list[j]))
-							index = j;
-					}
-					if (index == -1)
-						index = 0;
-				}
+				else
+					index++;
 			}
+			list[index].classList.add('highlight436255');
+			hl = document.getElementsByClassName('highlight436255')[0];
+			if (!isElementInViewport(hl))
+				hl.scrollIntoView(code == 90);
+			hl.querySelector(".text").select();
 		}
-		else if (code == 65 && index > 0) {
-			if (shift)
-				index = 0;
-			else
-				index--;
-		}
-		else if (code == 90 && index < list.length - 1) {
-			if (shift)
-				index = list.length - 1;
-			else
-				index++;
-		}
-		list[index].classList.add('highlight436255');
-		hl = document.getElementsByClassName('highlight436255')[0];
-		if (!isElementInViewport(hl))
-			hl.scrollIntoView(code == 90);
-		hl.querySelector(".text").select();
 	}
 	else if (code == 81 && hl != null) {
 		// q - select
@@ -163,7 +165,9 @@ function keyShortcuts(key) {
 	}
 	else if (code == 87 && hl != null) {
 		// w - prevent muscle memory
-		key.preventDefault();
+		if (!intext || (document.activeElement && document.activeElement.id.startsWith("favorites_"))) {
+			key.preventDefault();
+		}
 	}
 	else if (!intext && shift && code == 68) {
 		// shift+d - delete all
