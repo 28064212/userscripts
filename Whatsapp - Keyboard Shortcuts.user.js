b2e672522e95e53c00d3847d05a71c7b626a384d
28064212 <halligf2@gmail.com>
2024-09-21 16:54:08 +0100

    fix for server-side changes

--- Whatsapp - Keyboard Shortcuts.user.js
+++ Whatsapp - Keyboard Shortcuts.user.js
@@ -3,7 +3,7 @@
 // @namespace https://github.com/28064212/userscripts
 // @downloadURL https://github.com/28064212/userscripts/raw/master/Whatsapp%20-%20Keyboard%20Shortcuts.user.js
 // @include https://web.whatsapp.com/
-// @version 1.3.4
+// @version 1.3.5
 // @grant none
 // @inject-into content
 // ==/UserScript==
@@ -17,7 +17,7 @@ function keyShortcuts(key) {
 	let alt = key.altKey;
 	if (alt && (code == 40 || code == 38)) {
 		let chats = document.querySelectorAll('#pane-side > div > div > div > div');
-		let active = document.querySelector('div[aria-selected="true"]') == null ? null : document.querySelector('div[aria-selected="true"]').parentElement.parentElement;
+		let active = document.querySelector('#pane-side div[aria-selected="true"]') == null ? null : document.querySelector('#pane-side div[aria-selected="true"]').parentElement.parentElement;
 		let target = chats[0];
 		if (active == null) {
 			let lowest = getIndex(chats[0]);
@@ -52,11 +52,11 @@ function keyShortcuts(key) {
 			'bubbles': true,
 			'cancelable': true
 		});
-		document.querySelector('#side button:first-child').dispatchEvent(event);
+		document.querySelector('#side button').dispatchEvent(event);
 	}
 	else if (ctrl && code == 191) {
 		//message box - ctrl+/
-		document.querySelector('footer > div > div:nth-child(2)').focus();
+		document.querySelector('footer .lexical-rich-text-input').focus();
 	}
 }
 function getIndex(c) {
