0c43e85dfee47af85c69f0bf4389ef3e9081f02b
28064212 <28064212@users.noreply.github.com>
2025-02-13 18:52:25 +0000

    reset colour when navigating away from tab

--- Google Calendar - Keyboard Shortcuts.user.js
+++ Google Calendar - Keyboard Shortcuts.user.js
@@ -2,21 +2,26 @@
 // @name Google Calendar - Keyboard shortcuts
 // @namespace https://github.com/28064212/greasemonkey-scripts
 // @downloadURL https://github.com/28064212/greasemonkey-scripts/raw/master/Google%20Calendar%20-%20Keyboard%20Shortcuts.user.js
-// @version 0.1
+// @version 0.1.1
 // @description Use Ctrl to highlight current day
 // @match *://calendar.google.com/*
 // @grant none
 // ==/UserScript==
 
 if (window.top == window.self) {
-	window.addEventListener('keydown', keydown, true);
-	window.addEventListener('keyup', keyup, true);
+	window.addEventListener('keydown', keyDown, true);
+	window.addEventListener('keyup', keyUp, true);
+	document.addEventListener("visibilitychange", visibilityChange);
 }
-function keydown(key) {
+function keyDown(key) {
 	if (key.key == "Control")
 		document.querySelector('.F262Ye').style.backgroundColor = 'red';
 }
-function keyup(key) {
+function keyUp(key) {
 	if (key.key == "Control")
+		document.querySelector('.F262Ye').style.backgroundColor = '';
+}
+function visibilityChange() {
+	if (document.hidden)
 		document.querySelector('.F262Ye').style.backgroundColor = '';
 }
