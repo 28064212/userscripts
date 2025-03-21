c63996c7b8c516f775149ab8179271e39e7974bf
28064212 <28064212@users.noreply.github.com>
2025-03-21 14:40:50 +0000

    server-side changes

--- Google - Keyboard Shortcuts.user.js
+++ Google - Keyboard Shortcuts.user.js
@@ -3,7 +3,7 @@
 // @namespace https://github.com/28064212/userscripts
 // @downloadURL https://github.com/28064212/userscripts/raw/master/Google%20-%20Keyboard%20Shortcuts.user.js
 // @include /^https?://(www\.)?google\..*/search.*/
-// @version 1.6.1
+// @version 1.6.2
 // @description	a/z for up/down, q to open, ctrl-space to focus search
 // ==/UserScript==
 function addGlobalStyle(css) {
@@ -46,7 +46,7 @@ function keyShortcuts(key) {
 	let hl = document.getElementsByClassName('highlight436255')[0];
 	if (!isch && !intext && (code == 65 || code == 90)) {
 		// a/z - up/down
-		var list = document.querySelectorAll('.g, .MkXWrd, .JJZKK, .cIkxbf');
+		var list = document.querySelectorAll('#search > div > div > div:not(:empty)');
 		list = Array.from(list).filter(l => l.offsetParent != null);
 		if (hl != null)
 			hl.classList.remove('highlight436255');
